f4c1f6c3bada8a294d0837be6ad3cabcb74223a3
Dmitry Kulaga <dima.kulaga5@gmail.com>
2026-01-28 12:23:37 +0300

    Update ESLint and Jest configurations for improved rule handling and coverage

--- app/eslint.config.mjs
+++ app/eslint.config.mjs
@@ -5,12 +5,19 @@ import nextTs from "eslint-config-next/typescript";
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  {
+    files: ["jest.config.js"],
+    rules: {
+      "@typescript-eslint/no-require-imports": "off",
+    },
+  },
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
     ".next/**",
     "out/**",
     "build/**",
+    "coverage/**",
     "next-env.d.ts",
   ]),
 ]);
